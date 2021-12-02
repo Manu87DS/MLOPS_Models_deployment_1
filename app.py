635c7826460cf0495f6c049bdea06124fbe9c1ae
Emmanuel Bonnet <77730417+Manu87DS@users.noreply.github.com>
2021-12-02 16:24:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,6 @@ def predict_banknote(data:BankNote):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='192.168.1.100', port=8000) #######
+    uvicorn.run(app, host='127.0.0.1', port=8000) #######
     
 #uvicorn app:app --reload
