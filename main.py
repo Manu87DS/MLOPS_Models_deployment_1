79435001cba9b2c67d06602f0b790dcd7826fbfb
Emmanuel Bonnet <77730417+Manu87DS@users.noreply.github.com>
2021-12-02 16:25:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -30,5 +30,5 @@ def get_name(name: str):
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
 if __name__ == '__main__':
-    uvicorn.run(app, host='192.168.1.100', port=8000)
+    uvicorn.run(app, host='127.0.0.1', port=8000)
 #uvicorn main:app --reload
